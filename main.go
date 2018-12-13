3f511bff5bd4cd53aed293bf371a865ebf190d91
Michael Van Brummen <michaelvanbrummen@gmail.com>
2018-12-13 21:03:03 +1000

    Added comments

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
+// A AttrMap is a html attribute map type
 type AttrMap = map[string]string
 
+// A Node represents a html node
 type Node struct {
 	Children []*Node
 	NodeType interface{}
 }
 
+// An Element represents a html element node type
 type Element struct {
 	TagName    string
 	Attributes AttrMap
